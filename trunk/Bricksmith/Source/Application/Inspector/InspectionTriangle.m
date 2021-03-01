af9900ded2b6ede53b9797baa93ad38da2935d71
Allen Smith <s164276@yahoo.com>
2021-02-28 22:55:16 -0800

    Remove NSForm in Triangle inspector

--- trunk/Bricksmith/Source/Application/Inspector/InspectionTriangle.m
+++ trunk/Bricksmith/Source/Application/Inspector/InspectionTriangle.m
@@ -11,9 +11,9 @@
 //==============================================================================
 #import "InspectionTriangle.h"
 
+#import "LDrawColorWell.h"
 #import "LDrawModel.h"
 #import "LDrawTriangle.h"
-#import "FormCategory.h"
 
 @implementation TriangleView
 
@@ -42,6 +42,27 @@
 @end
 
 
+// MARK: -
+
+@interface InspectionTriangle ()
+
+@property (nonatomic, unsafe_unretained) IBOutlet	LDrawColorWell*	colorWell;
+
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex1XField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex1YField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex1ZField;
+
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex2XField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex2YField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex2ZField;
+
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex3XField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex3YField;
+@property (nonatomic, unsafe_unretained) IBOutlet	NSTextField*	vertex3ZField;
+
+@end
+
+
 @implementation InspectionTriangle
 
 //========== init ==============================================================
@@ -62,6 +83,29 @@
 }//end init
 
 
+// MARK: - ACCESSORS -
+
+//========== vertex1Fields =====================================================
+//==============================================================================
+- (NSArray<NSTextField*>*) vertex1Fields
+{
+	return @[_vertex1XField, _vertex1YField, _vertex1ZField];
+}
+
+//========== vertex2Fields =====================================================
+//==============================================================================
+- (NSArray<NSTextField*>*) vertex2Fields
+{
+	return @[_vertex2XField, _vertex2YField, _vertex2ZField];
+}
+
+//========== vertex3Fields =====================================================
+//==============================================================================
+- (NSArray<NSTextField*>*) vertex3Fields
+{
+	return @[_vertex3XField, _vertex3YField, _vertex3ZField];
+}
+
 #pragma mark -
 #pragma mark ACTIONS
 #pragma mark -
@@ -75,10 +119,10 @@
 {
 	LDrawTriangle *representedObject = [self object];
 	
-	Point3 vertex1 = [vertex1Form coordinateValue];
-	Point3 vertex2 = [vertex2Form coordinateValue];
-	Point3 vertex3 = [vertex3Form coordinateValue];
-	
+	Point3	vertex1 = [self coordinateValueFromFields:[self vertex1Fields]];
+	Point3	vertex2 = [self coordinateValueFromFields:[self vertex2Fields]];
+	Point3	vertex3 = [self coordinateValueFromFields:[self vertex3Fields]];
+
 	[representedObject setVertex1:vertex1];
 	[representedObject setVertex2:vertex2];
 	[representedObject setVertex3:vertex3];
@@ -91,8 +135,8 @@
 //========== revert ============================================================
 //
 // Purpose:		Restores the palette to reflect the state of the object.
-//				This method is called automatically when the object to inspect 
-//				is set. Subclasses should override this method to populate 
+//				This method is called automatically when the object to inspect
+//				is set. Subclasses should override this method to populate
 //				the data in their inspector palettes.
 //
 //==============================================================================
@@ -100,16 +144,16 @@
 {
 	LDrawTriangle *representedObject = [self object];
 
-	[colorWell setLDrawColor:[representedObject LDrawColor]];
+	[_colorWell setLDrawColor:[representedObject LDrawColor]];
 
 	Point3 vertex1 = [representedObject vertex1];
 	Point3 vertex2 = [representedObject vertex2];
 	Point3 vertex3 = [representedObject vertex3];
 	
-	[vertex1Form setCoordinateValue:vertex1];
-	[vertex2Form setCoordinateValue:vertex2];
-	[vertex3Form setCoordinateValue:vertex3];
-	
+	[self setCoordinateValue:vertex1 onFields:[self vertex1Fields]];
+	[self setCoordinateValue:vertex2 onFields:[self vertex2Fields]];
+	[self setCoordinateValue:vertex3 onFields:[self vertex3Fields]];
+
 	[super revert:sender];
 	
 }//end revert:
@@ -119,57 +163,63 @@
 
 //========== vertex1EndedEditing: ==============================================
 //
-// Purpose:		The user had been editing the coordinate; now he has stopped. 
-//				We need to find out if he actually changed something. If so, 
+// Purpose:		The user had been editing the coordinate; now he has stopped.
+//				We need to find out if he actually changed something. If so,
 //				update the object.
 //
 //==============================================================================
 - (IBAction) vertex1EndedEditing:(id)sender
 {
-	Point3 formContents	= [vertex1Form coordinateValue];
+	Point3 formContents	= [self coordinateValueFromFields:[self vertex1Fields]];
 	Point3 vertex1		= [[self object] vertex1];
 	
 	//If the values really did change, then update.
 	if(V3EqualPoints(formContents, vertex1) == NO)
+	{
 		[self finishedEditing:sender];
+	}
 		
 }//end vertex1EndedEditing:
 
 
 //========== vertex2EndedEditing: ==============================================
 //
-// Purpose:		The user had been editing the coordinate; now he has stopped. 
-//				We need to find out if he actually changed something. If so, 
+// Purpose:		The user had been editing the coordinate; now he has stopped.
+//				We need to find out if he actually changed something. If so,
 //				update the object.
 //
 //==============================================================================
 - (IBAction) vertex2EndedEditing:(id)sender
 {
-	Point3 formContents	= [vertex2Form coordinateValue];
+	Point3 formContents	= [self coordinateValueFromFields:[self vertex2Fields]];
 	Point3 vertex2		= [[self object] vertex2];
 	
 	//If the values really did change, then update.
 	if(V3EqualPoints(formContents, vertex2) == NO)
+	{
 		[self finishedEditing:sender];
+	}
 		
 }//end vertex2EndedEditing:
 
 
 //========== vertex3EndedEditing: ==============================================
 //
-// Purpose:		The user had been editing the coordinate; now he has stopped. 
-//				We need to find out if he actually changed something. If so, 
+// Purpose:		The user had been editing the coordinate; now he has stopped.
+//				We need to find out if he actually changed something. If so,
 //				update the object.
 //
 //==============================================================================
 - (IBAction) vertex3EndedEditing:(id)sender
 {
-	Point3 formContents	= [vertex3Form coordinateValue];
+	Point3 formContents	= [self coordinateValueFromFields:[self vertex3Fields]];
 	Point3 vertex3		= [[self object] vertex3];
 	
 	//If the values really did change, then update.
 	if(V3EqualPoints(formContents, vertex3) == NO)
+	{
 		[self finishedEditing:sender];
+	}
 		
 }//end vertex3EndedEditing:
 
